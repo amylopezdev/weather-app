4eec6167c1c92ffc01ea6e7ba94538bffc554a6f
Amy Lopez <amy.lopez@sainsburysbank.co.uk>
2025-10-20 18:40:50 +0100

    add custom hook for fetching data

---
+++ src/hooks/useFetchData.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+export const useFetchData = (city, url) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [data, setData] = useState();
+
+  const getWeather = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      const result = await response.json();
+      setData(result);
+      console.log(result.list);
+    } catch (error) {
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getWeather();
+  }, [city]);
+
+  return { isLoading, isError, data };
+};
