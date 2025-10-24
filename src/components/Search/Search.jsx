52c76a17b4317fdd0cdd1d78dc552871a6c957fb
Amy Lopez <amy.lopez@sainsburysbank.co.uk>
2025-10-24 10:54:00 +0100

    add error message in search

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -5,16 +5,24 @@ import "./Search.css";
 export const Search = () => {
   const { setCity } = useContext(CityContext);
   const [searchInput, setSearchInput] = useState("");
+  const [infoText, setInfoText] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setCity(searchInput);
+    if (searchInput) {
+      setCity(searchInput);
+      setInfoText()
+    }
+    if (!searchInput) {
+      setInfoText("Please type something");
+    }
   };
 
   return (
     <section className="search">
       <form onSubmit={handleSubmit}>
         <label htmlFor="search">Search a city name</label>
+        <div className="info-text">{infoText}</div>
         <div className="search-bar">
           <input
             id="search"
